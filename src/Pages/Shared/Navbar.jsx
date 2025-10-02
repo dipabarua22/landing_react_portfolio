ffc2e6d41cd50993f05df33681190915d2583450
dipa <dipabarua44@gmail.com>
2025-10-02 13:23:39 +0600

    update

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -72,7 +72,7 @@ const Navbar = () => {
 
       {/* Mobile dropdown */}
       {isOpen && (
-        <div className="md:hidden bg-white dark:bg-black shadow-lg flex flex-col items-center space-y-4 py-6">
+        <div className="md:hidden bg-white dark:bg-orange-500 shadow-lg flex flex-col items-center space-y-4 py-6">
           <Link onClick={toggleMenu} to="home" smooth duration={500}  className="cursor-pointer hover:text-yellow-500 dark:text-white">Home</Link>
           <Link onClick={toggleMenu} to="about" smooth duration={500}   className="cursor-pointer hover:text-yellow-500 dark:text-white">About</Link>
           <Link onClick={toggleMenu} to="services" smooth duration={500}  className="cursor-pointer hover:text-yellow-500 dark:text-white">Services</Link>
